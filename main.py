71cd58778f9090463ba6b17d349e0cba1f58cf99
LosEvons <emil.malk@gmail.com>
2026-03-19 12:42:45 +0200

    Created some utility classes and functions

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import argparse
 from dataclasses import dataclass
 from enum import Enum, auto
+from itertools import batched
+
 
 class CellType(Enum):
     SOLID = auto()
     FREE = auto()
+
+
+cell_chars = {
+        CellType.SOLID: "#",
+        CellType.FREE:  " ",
+    }
 
 
 @dataclass
@@ -12,6 +20,20 @@ class Matrix2D:
     w: int
     h: int
     cells: list[CellType]
+    
+
+def matrix2d_to_string(matrix2d: Matrix2D) -> str:
+    if len(matrix2d.cells) != matrix2d.w * matrix2d.h:
+        raise ValueError("incorrect matrix2d cell array length for given dimensions")
+    
+    return "\n".join(
+        "".join(cell_chars[cell] for cell in row)
+        for row in batched(matrix2d.cells, matrix2d.w)
+    )
+
+
+def print_matrix2d(matrix2d: Matrix2D):
+    print(matrix2d_to_string(matrix2d))
     
 
 def main(w: int, h: int, seed: int | None):
